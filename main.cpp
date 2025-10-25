9901147617d5c4d6ffd9e17aefcd61134f41982e
anyable <stupiranna@mail.ru>
2025-10-25 13:18:33 +0300

    add start input  b, c

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ bool isPyth(unsigned a, unsigned b, unsigned c){
 int main()
 {
   using u_t = unsigned;
-  u_t a = 0, b = 0, c = 0;
+  u_t a, c, b;
+  std::cin >> c >> b;
   size_t count = 0;
   while (std::cin >> a){
     count += isPyth(a, b, c) ? 1 : 0;
